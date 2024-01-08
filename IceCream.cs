43e4c12c778eaa1b25ed2c94b0d3ee194a0ae43b
chloe <s10256056@connect.np.edu.sg>
2024-01-08 14:43:27 +0800

    added changes to class IceCream

--- IceCream.cs
+++ IceCream.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PRG2_Assignment2_Team12
 {
-    internal class IceCream
+    class IceCream
     {
+        public string Option { get; set; }
+        public int Scoops { get; set; }
+        public List<Flavour> Flavours { get; set; } = new List<Flavour>();
+        public List<Topping> Toppings { get; set; } = new List<Topping>();
+        public IceCream() { }
+        public IceCream(string option, int scoops)
+        {
+            Option = option;
+            Scoops = scoops;
+        }
     }
 }
