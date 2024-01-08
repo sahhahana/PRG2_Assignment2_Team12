3bc5d36b45b08499a206f68c2c0518265b0350d9
Sahhahana <s10258439@connect.np.edu.sg>
2024-01-08 13:11:17 +0800

    added properties

--- Customer.cs
+++ Customer.cs
@@ -9,7 +9,15 @@ namespace PRG2_Assignment2_Team12
     internal class Customer
     {
         private string name;
+        private int memberid;
+        private DateTime dob;
+        private List<Order> orderHistory;
+        private PointCard rewards;
         public string Name { get; set; }
+        public int Memberid { get; set; }
+        public DateTime Dob { get; set; }
+        public List<Order> OrderHistory { get; set; } = new List<Order>();
+        public PointCard Rewards { get; set; }
 
     }
 }
