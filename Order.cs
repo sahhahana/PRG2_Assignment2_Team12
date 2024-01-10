c3103acc5f4d3226be3eae04d43a508e2ee99f47
Sahhahana <s10258439@connect.np.edu.sg>
2024-01-10 23:08:37 +0800

    completed until Order()

--- Order.cs
+++ Order.cs
@@ -8,5 +8,23 @@ namespace PRG2_Assignment2_Team12
 {
     internal class Order
     {
+        private int id;
+        private DateTime timeReceived;
+        private DateTime? timeFulfilled;
+        private List<IceCream> iceCreamList;
+        public int Id { get;set; }
+        public DateTime TimeReceived { get; set; }
+        public DateTime? TimeFulfilled { get; set;}
+        public List<IceCream> IceCreamList { get; set; } = new List<IceCream>();
+        public Order() { }
+        public Order(int id, DateTime timeReceived)
+        {
+            Id = id;
+            TimeReceived = timeReceived;
+        }
+        public ModifyIceCream(int id)
+        {
+
+        }
     }
 }
