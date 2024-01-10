f1c2d8b62a36af774627826ebead009077d2b66f
Sahhahana <s10258439@connect.np.edu.sg>
2024-01-10 22:23:11 +0800

    pointCard class cpompleted - need to check the methods all

--- PointCard.cs
+++ PointCard.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,35 @@ namespace PRG2_Assignment2_Team12
 {
     internal class PointCard
     {
+        private int points;
+        private int punchCard;
+        private string tier;
+        public int Points { get; set; }
+        public int PunchCard { get; set;}
+        public string Tier { get; set; }
+        public PointCard() { }
+        public PointCard(int points, int punchCard)
+        {
+            Points = points;
+            PunchCard = punchCard;
+            
+        }
+        public void AddPoints(int points)
+        {
+            PunchCard += points;
+        }
+        public void RedeemPoints(int points)
+        {
+            if (Tier != "Ordinary")
+            {
+                PunchCard -= points;
+            }
+            
+        }
+        public void Punch()
+        {
+            PunchCard = 0;
+        }
+
     }
 }
