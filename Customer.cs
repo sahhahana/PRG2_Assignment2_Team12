1aeba45a5faf3547e4ea550c7926ecb75accb703
Sahhahana <s10258439@connect.np.edu.sg>
2024-01-10 21:58:25 +0800

    completed customer class

--- Customer.cs
+++ Customer.cs
@@ -11,13 +11,39 @@ namespace PRG2_Assignment2_Team12
         private string name;
         private int memberid;
         private DateTime dob;
+        private Order currentOrder;
         private List<Order> orderHistory;
         private PointCard rewards;
         public string Name { get; set; }
         public int Memberid { get; set; }
         public DateTime Dob { get; set; }
-        public List<Order> OrderHistory { get; set; } = new List<Order>();
-        public PointCard Rewards { get; set; }
+        public Order CurrentOrder { get; set; }
 
+        public List<Order> OrderHistory { get; set; } = new List<Order>();
+        public PointCard Rewards { get; set; } = new PointCard();
+        public Customer() { }
+        public Customer(string name, int memberid, DateTime dob)
+        {
+            Name = name;
+            Memberid = memberid;
+            Dob = dob;           
+        }
+        public Order MakeOrder()
+        {
+            Order order = new Order();
+            return order;
+        }
+        public bool IsBirthday()
+        {
+            if (Dob == DateTime.Today) 
+            { 
+                return true;
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()} Name: {Name} Member ID: {Memberid} DOB: {Dob}";
+        }
     }
 }
