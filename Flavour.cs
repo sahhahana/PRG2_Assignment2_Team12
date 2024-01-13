bb3dd36ba784c1a68ccece4e9b08ac7d8d9526cd
Sahhahana <s10258439@connect.np.edu.sg>
2024-01-13 20:11:41 +0800

    completed class

--- Flavour.cs
+++ Flavour.cs
@@ -8,5 +8,22 @@ namespace PRG2_Assignment2_Team12
 {
     internal class Flavour
     {
+        private string type;
+        private bool premium;
+        private int quantity;
+        public string Type { get; set; }    
+        public bool Premium { get; set; }
+        public int Quantity { get; set; }
+        public Flavour() { }
+        public Flavour(string type, bool premium, int quantity)
+        {
+            Type = type;
+            Premium = premium;
+            Quantity = quantity;
+        }
+        public override string ToString()
+        {
+            return
+        }
     }
 }
--- Topping.cs
+++ Topping.cs
@@ -8,5 +8,16 @@ namespace PRG2_Assignment2_Team12
 {
     internal class Topping
     {
+        private string type;
+        public string Type { get; set; }    
+        public Topping() { }
+        public Topping(string type)
+        {
+            Type = type;
+        }
+        public override string ToString()
+        {
+            return $"Type: {Type}";
+        }
     }
 }
