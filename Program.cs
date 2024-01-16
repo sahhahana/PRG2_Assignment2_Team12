abeea0f79d989021718107ba14999ff204f4ecd5
Sahhahana <s10258439@connect.np.edu.sg>
2024-01-16 16:17:04 +0800

    read names in customers.csv

--- Program.cs
+++ Program.cs
@@ -4,12 +4,25 @@
 // Partner Name : Sahana
 //==========================================================
 using PRG2_Assignment2_Team12;
-Console.WriteLine("Hello World");
+
 
 //chloe - features 2,5 and 6
 //sahana- features 1, 3 ad 4
 
 // Feature 1
+using (StreamReader sr = new StreamReader("customers.csv"))
+{
+    while (!sr.EndOfStream)
+    {
+        string line = sr.ReadLine();
+        if (line != null)
+        {
+            string[] data = line.Split(",");
+            Console.WriteLine(data[0]);
+        }
+    }
+}
+
 
 // Feature 2
 
@@ -27,4 +40,3 @@ Console.WriteLine("Hello World");
 
 // Advanced (c)
 
-Console.WriteLine("hello world");
