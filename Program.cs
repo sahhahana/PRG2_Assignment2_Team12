e98352179997b1aa9b126ddb45a2b0db36cfc65c
chloe <s10256056@connect.np.edu.sg>
2024-01-29 22:17:13 +0800

    ll

--- Program.cs
+++ Program.cs
@@ -1260,7 +1260,6 @@ amounts for the input year
                 if (timeFulfilled.Year == year && timeFulfilled.Month == i)
                 {
                     double monthlyPrice = order.CalculateTotal();
-                    Console.WriteLine(order.CalculateTotal());
                     yearlyTotal += monthlyPrice;
                     monthlyTotal += monthlyPrice;
                 }
